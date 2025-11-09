df74ae5f3eba39c87b8bd93cbd3cb584a184415a
ISellSoup <willschaller@icloud.com>
2025-11-08 21:09:20 -0600

    Refactor and extend class hierarchy for sets
    Replaces AnswerBank with a more flexible Collection-based hierarchy, introducing Collection, SubCollection, WordBankEntry, WordBank, Group, Folder, and Set classes. Adds conversion methods and properties for better structure and extensibility, supporting nested collections and batch operations.

--- scripts/setClasses.js
+++ scripts/setClasses.js
@@ -1,26 +1,94 @@
-class AnswerBank {
-    constructor(answers = []) {
-        this.answers = answers;
+class Collection {
+    constructor(contents = []) {
+        this.contents = contents;
+        this.displayName = "";
+    }
+}
+
+class SubCollection extends Collection {
+    constructor(contents = []) {
+        super(contents);
+        this.parent = null;
+    }
+}
+
+class WordBankEntry {
+    constructor(value = "") {
+        this.value = value;
+        this.parent = null;
+        this.enabled = true;
+    }
+    convertToWordBank() {
+        return new WordBank([this]); // Make sure to let the user name the wordbank immediately after conversion
+    }
+}
+
+class WordBank extends SubCollection {
+    constructor(contents = []) {
+        super(contents);
+        this.enabled = true; // This property doesn't work on top-level WordBanks
+    }
+    isTopLevel() {
+        let isTopLevel = true;
+
+        function checkParent(item) {
+            if (item instanceof WordBank) isTopLevel = false;
+
+            if (item.parent) {
+                checkParent(item.parent);
+            }
+        }
+        checkParent(this.parent);
+        return isTopLevel;
     }
 }
 
 class QuestionAsnwerPair {
     constructor(question = "", answer = "") {
         this.question = question;
+        // Can be a string or a WordBank
         this.answer = answer;
+        this.parent = null;
+        this.enabled = true;
     }
 }
 
-class Group {
+// Groups of other Groups or objects, used for batch enable/disable. The top level of a WordBank cannot be put in a Group.
+class Group extends SubCollection {
     constructor(contents = []) {
-        this.contents = contents;
+        super(contents);
+        this.enabled = true;
+    }
+    convertToFolder() {
+        const folder = new Folder(this.contents);
+        folder.displayName = this.displayName;
+        return folder;
     }
 }
 
-class Set {
+// Folders can contain any objects, but do not have an enabled property.
+class Folder extends SubCollection {
+    constructor(contents = []) {
+        super(contents);
+    }
+    convertToGroup() {
+        this.contents.forEach(item => {
+            if (item instanceof WordBank) {
+                if (item.isTopLevel()) return; // Groups cannot contain top-level WordBanks
+            }
+        });
+        const group = new Group(this.contents);
+        group.displayName = this.displayName;
+        return group;
+    }
+}
+
+class Set extends Collection {
     constructor() {
-        this.displayName = "";
+        super([]);
         this.description = "";
-        this.data = [];
+
+        // Indexes to objects in contents or direct objects (Groups count as objects)
+        this.body = [];
     }
 }
